44d935b0bbdaad6e811f48c88cb25978dad1770b
Zarrar Khan <zarrar.khan@pnnl.gov>
2023-02-11 21:38:09 -0500

    Updating app backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsfullstack": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
